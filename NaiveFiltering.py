286f9e8d246f095317471e1a1f9d764d90e1f21f
JamesFW <james.wu@students.olin.edu>
2013-05-07 23:13:40 -0400

    Band pass filtering with the vocal range suggested in a research paper

--- NaiveFiltering.py
+++ NaiveFiltering.py
@@ -2,9 +2,9 @@ from Analysis import *
 import numpy
 
 
-def main(start=60, stop=90):
-    highCutoff = 0
-    lowCutoff = 1000
+def main(start=60, stop=70):
+    highCutoff = 2048
+    lowCutoff = 512
     vocalBoostLowCutoff = 20000
     vocalBoostHighCutoff = 1000
     boostFactor = 2
@@ -12,11 +12,12 @@ def main(start=60, stop=90):
     yesterday = SongModifier('./samples/yesterday.wav')
     FFT = yesterday.partialRealFFT(start, stop)
     bandPassedSignal = yesterday.bandStopFFT(FFT, lowCutoff, highCutoff)
-    vocalBoostSignal = yesterday.bandBoostFFT(bandPassedSignal, vocalBoostLowCutoff, vocalBoostHighCutoff, boostFactor)
-    IFFT = yesterday.inverseRealFFT(vocalBoostSignal)
+    #vocalBoostSignal = yesterday.bandBoostFFT(bandPassedSignal, vocalBoostLowCutoff, vocalBoostHighCutoff, boostFactor)
+
+    IFFT = yesterday.inverseRealFFT(bandPassedSignal)
     yesterday.writeWAV(IFFT, './processed/naiveFilter2.wav')
     yesterday.plotPartialSpectrogram(10, 25, yesterday.data, tempo=120)
     yesterday.plotSpectrogram(IFFT, tempo=120)
 
 if __name__ == "__main__":
-    main(10, 25)
+    main(10, 20)
